1207dbf47ea275e0660fe0a1182510749a5106ea
Rachel T <rachmaybe@gmail.com>
2019-09-01 04:00:03 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    self.split.count
-    self.count_sentences
+   new_array = self.split(/[.?!]/).reject {|string| string.empty?}
+    return new_array.length
   end
 end
